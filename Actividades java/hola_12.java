67a43c4e8b1320f5ad230a9743f9df32932c3705
Alejandro <alejandrocarmona12831@gmail.com>
2021-10-27 10:16:21 -0500

    Actividades 3

---
+++ Actividades java/hola_12.java
@@ -0,0 +1,24 @@
+package holamundo;
+
+/**
+ *
+ * @author Elvis
+ */
+public class hola_12 {
+    public static void main(String[] args) {
+        int a = 3, b=2;
+        int c = a + 5 - b; 
+        System.out.println("c = " + c);
+        
+        a += 1;//a = a + 1
+        System.out.println("a = " + a);
+        
+        a += 3;//a = a + 3
+        System.out.println("a = " + a);
+        
+        a -= 2;//a = a - 2
+        System.out.println("a = " + a);
+        
+        // *=   /=   %=
+    }
+}
---
+++ Actividades java/hola_17.java
@@ -0,0 +1,16 @@
+package holamundo;
+
+/**
+ *
+ * @author Elvis
+ */
+public class hola_17 {
+    public static void main(String[] args) {
+        var resultado = (1 > 2) ? "verdadero" : "falso";        
+        System.out.println("resultado = " + resultado);
+        
+        var numero = 9;
+        resultado = (numero % 2 == 0) ? "numero par" : "numero impar";
+        System.out.println("resultado = " + resultado);
+    }    
+}
